4736c324310824d9578a5959d148a3ecaae89a3b
josseph caceres <josseph501.djcp@gmail.com>
2019-07-12 16:31:03 -0500

    CB-01-prueba-tecnica-original
    se reali<a el primer commit donde se sube todoel proyecto original sin cambios.

---
+++ Biblioteca.net/BibliotecaDominio/Bibliotecario.cs
@@ -0,0 +1,31 @@
+﻿using BibliotecaDominio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaDominio
+{
+    public class Bibliotecario
+    {
+        public const string EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE = "El libro no se encuentra disponible";
+        private  IRepositorioLibro libroRepositorio;
+        private  IRepositorioPrestamo prestamoRepositorio;
+
+        public Bibliotecario(IRepositorioLibro libroRepositorio, IRepositorioPrestamo prestamoRepositorio)
+        {
+            this.libroRepositorio = libroRepositorio;
+            this.prestamoRepositorio = prestamoRepositorio;
+        }
+
+        public void Prestar(string isbn, string nombreUsuario)
+        {
+            throw new Exception("se debe implementar este método");
+        }
+
+
+        public bool EsPrestado(string isbn)
+        {
+            return false;
+        }
+    }
+}
---
+++ Biblioteca.net/DominioTest/Integracion/BibliotecarioTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BibliotecaDominio;
+using BibliotecaRepositorio.Contexto;
+using BibliotecaRepositorio.Repositorio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DominioTest.TestDataBuilders;
+using Microsoft.EntityFrameworkCore;
+
+namespace DominioTest.Integracion
+{
+
+    [TestClass]
+    public class BibliotecarioTest
+    {
+        public const String CRONICA_UNA_MUERTE_ANUNCIADA = "Cronica de una muerte anunciada";
+        private  BibliotecaContexto contexto;
+        private  RepositorioLibroEF repositorioLibro;
+        private RepositorioPrestamoEF repositorioPrestamo;
+
+
+        [TestInitialize]
+        public void setup()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BibliotecaContexto>();
+            contexto = new BibliotecaContexto(optionsBuilder.Options);
+            repositorioLibro  = new RepositorioLibroEF(contexto);
+            repositorioPrestamo = new RepositorioPrestamoEF(contexto, repositorioLibro);
+        }
+
+        [TestMethod]
+        public void PrestarLibroTest()
+        {
+            // Arrange
+            Libro libro = new LibroTestDataBuilder().ConTitulo(CRONICA_UNA_MUERTE_ANUNCIADA).Build();
+            repositorioLibro.Agregar(libro);
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+
+            // Act
+            bibliotecario.Prestar(libro.Isbn, "Juan");
+
+            // Assert
+            Assert.AreEqual(bibliotecario.EsPrestado(libro.Isbn), true);
+            Assert.IsNotNull(repositorioPrestamo.ObtenerLibroPrestadoPorIsbn(libro.Isbn));
+
+        }
+
+        [TestMethod]
+        public void PrestarLibroNoDisponibleTest()
+        {
+            // Arrange
+            Libro libro = new LibroTestDataBuilder().ConTitulo(CRONICA_UNA_MUERTE_ANUNCIADA).Build();
+            repositorioLibro.Agregar(libro);
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+
+            // Act
+            bibliotecario.Prestar(libro.Isbn,"Juan");
+            try
+            {
+                bibliotecario.Prestar(libro.Isbn, "Juan");
+                Assert.Fail();
+            }
+            catch (Exception err)
+            {
+                // Assert
+                Assert.AreEqual("El libro no se encuentra disponible", err.Message);
+            }
+        
+        }
+
+    }
+}
